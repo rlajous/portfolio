12ae56645d3ee381053878d8a451e6399e0647ee
Navi <rodrigo@lajous.com.ar>
2020-05-31 23:36:30 -0300

    Repo init

---
+++ components/Nav.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav class="nav">
+    <nuxt-link to="/">
+      <picture>
+          <!-- <source media="(min-width:465px)" srcset="img_white_flower.jpg"> -->
+          <img src="~/assets/img/logo.png" alt="My Logo, a tree inside a circle" class="logo">
+      </picture>
+    </nuxt-link>
+    <div class="links">
+      <nuxt-link to="/" class="text" exact-active-class="active">
+        Home
+      </nuxt-link>
+      <nuxt-link to="/about" class="text" exact-active-class="active">
+        About Me
+      </nuxt-link>
+      <nuxt-link to="/portfolio" class="text" exact-active-class="active">
+        My Portfolio
+      </nuxt-link>
+      <nuxt-link to="/recomendations" class="text" exact-active-class="active">
+        Recomendations
+      </nuxt-link>
+      <nuxt-link to="/contact" class="button">
+        Contact Me
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+
+.nav {
+  height: 120px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 10%;
+  justify-content: space-between;
+
+  .logo {
+    height: 100px;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 60%;
+    justify-content: space-between;
+
+    .text {
+      // width: 97px;
+      height: 30px;
+      // font-family: FuturaHvBT;
+      font-size: 22px;
+      font-weight: 900;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.36;
+      letter-spacing: normal;
+      text-align: left;
+      color: black;
+      text-decoration: none;
+
+      &.active {
+          color: #11cdef;
+          position: relative;
+
+          &::after {
+            content: "";
+              display: block;
+              position: absolute;
+              bottom: -17px;
+              width: 100%;
+              height: 1.5px;
+              background-color: #11cdef;
+          }
+      }
+    }
+
+    .button {
+      width: 214px;
+      height: 50px;
+      border-radius: 10px;
+      background-image: linear-gradient(to left, #8eeeff, #11cdef);
+      text-decoration: none;
+      line-height: 50px;
+      font-size: 22px;
+      font-weight: 900;
+      font-stretch: normal;
+      font-style: normal;
+      // line-height: 1.36;
+      letter-spacing: normal;
+      text-align: center;
+      color: white;
+    }
+  }
+}
+
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,335 @@
+<template>
+  <div class="container">
+    <h1 class="title">
+      Contact Me
+    </h1>
+    <h2 class="subtitle">
+      Turn Your Ideas into Reality with me
+      <br>
+      Feel free to Contact me any time
+    </h2>
+    <div class="content">
+      <form action="" class="form">
+        <input
+          class="input"
+          type='text'
+          name='name'
+          tabindex="3"
+          placeholder="Full Name"
+          required
+          v-model="name">
+        <input
+          class="input"
+          type='email'
+          name='email'
+          tabindex="3"
+          placeholder="Email"
+          required
+          v-model="email">
+        <textarea
+          class="textarea"
+          type='text'
+          name='message'
+          tabindex="3"
+          required
+          placeholder="Tell me how can I help you"
+          v-model="message"></textarea>
+        <button
+          class="button"
+          type='submit'
+          id="contact-submit"
+          name="submit"
+          @click.stop.prevent="sendEmail()">
+          Send Now
+        </button>
+      </form>
+      <div class="info">
+        <p class="info-title">
+          You Can also Send Me Message Via
+        </p>
+        <!-- <a href="" class="link">
+          <picture>
+            <img src="~/assets/img/github-color.png" alt="My Logo, a tree inside a circle" class="logo">
+          </picture>
+          <span class="text">
+            /rlajous
+          </span>
+        </a> -->
+        <a href="https://www.linkedin.com/in/rodrigo-manuel-navarro-lajous/" class="link" target="_blank">
+          <picture>
+            <img src="~/assets/img/linkedin-color.png" alt="My Logo, a tree inside a circle" class="logo">
+          </picture>
+          <span class="text">
+            /rodrigo-manuel-navarro-lajous
+          </span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { page } from 'vue-analytics'
+
+export default {
+  name: 'Contact',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    track() {
+      page('/contact')
+    },
+    async sendEmail() {
+      const { name, surname, email, message } = this
+      const ok = await await this.$axios.$post(
+        `https://us-central1-contacto-landing.cloudfunctions.net/sendMail?name=${name}&email=${email}&message=${message}`
+      )
+      if ( ok === 'Sended') {
+        // show a message sended
+      }
+    }
+  },
+  head() {
+    return {
+      title: 'Contacto',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: ''
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            ''
+        },
+        {
+          hid: 'title',
+          name: 'title',
+          content: ''
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: ''
+        },
+        {
+          hid: 'og:site_name',
+          property: 'og:site_name',
+          content: ''
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: ''
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: ''
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: ''
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: ''
+        },
+        {
+          hid: 'twitter:card',
+          property: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        {
+          hid: 'twitter:url',
+          property: 'twitter:url',
+          content: ''
+        },
+        {
+          hid: 'twitter:title',
+          property: 'twitter:title',
+          content: ''
+        },
+        {
+          hid: 'twitter:description',
+          property: 'twitter:description',
+          content: ''
+        },
+        {
+          hid: 'twitter:image',
+          property: 'twitter:image',
+          content: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .title {
+    // width: 626px;
+    height: 166px;
+    opacity: 0.08;
+    // font-family: Impact;
+    font-size: 136px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.22;
+    letter-spacing: normal;
+    text-align: left;
+    color: var(--black);
+  }
+
+  .subtitle {
+    position: relative;
+    top: -65px;
+    // width: 1126px;
+    height: 156px;
+    // font-family: Futura;
+    font-size: 32px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.34;
+    letter-spacing: normal;
+    text-align: center;
+    color: var(--black);
+  }
+
+  .content {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-around;
+    margin-bottom: 10%;
+
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      width: 674px;
+
+      .input {
+        width: 100%;
+        height: 85px;
+        border-radius: 10px;
+        border: solid 0.3px #707070;
+        background-color: var(--white);
+        padding-left: 24px;
+        // font-family: FuturaLtBT;
+        font-size: 25px;
+        font-weight: 300;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 85px;
+        letter-spacing: normal;
+        text-align: left;
+        color: var(--black);
+        margin-bottom: 20px;
+      }
+
+      .textarea {
+        padding: 24px;
+        width: 100%;
+        height: 197px;
+        border-radius: 10px;
+        border: solid 0.3px #707070;
+        background-color: var(--white);
+        // font-family: FuturaLtBT;
+        font-size: 25px;
+        font-weight: 300;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.32;
+        letter-spacing: normal;
+        text-align: left;
+        color: var(--black);
+        margin-bottom: 40px;
+      }
+
+      .button {
+        width: 306px;
+        height: 66px;
+        border-radius: 10px;
+        background-color: var(--red-pink);
+        border: none;
+        // font-family: FuturaHvBT;
+        font-size: 25px;
+        font-weight: 900;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 66px;
+        letter-spacing: normal;
+        text-align: center;
+        color: var(--white);
+      }
+    }
+
+    .info {
+      display: flex;
+      flex-direction: column;
+
+      .info-title {
+        // width: 657px;
+        height: 53px;
+        // font-family: Futura;
+        font-size: 40px;
+        font-weight: bold;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.33;
+        letter-spacing: normal;
+        text-align: left;
+        color: var(--black);
+        margin-bottom: 40px;
+      }
+
+      .link {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-bottom: 30px;
+        text-decoration: none;
+
+        .logo {
+          width: 50px;
+          height: 50px;
+          object-fit: contain;
+        }
+
+        .text {
+          height: 40px;
+          // font-family: FuturaLtBT;
+          font-size: 30px;
+          font-weight: 300;
+          font-stretch: normal;
+          font-style: normal;
+          line-height: 1.33;
+          letter-spacing: normal;
+          text-align: left;
+          color: var(--black);
+        }
+      }
+    }
+  }
+
+
+}
+</style>
