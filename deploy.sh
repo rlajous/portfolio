0020e92a20ed3825044869e7368181d83843326a
Navi <rodrigo@lajous.com.ar>
2020-06-06 20:18:18 -0300

    Deleted no need code

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID= process.env.AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY= process.env.AWS_SECRET_ACCESS_KEY
-export AWS_BUCKET_NAME= process.env.AWS_BUCKET_NAME
-export AWS_CLOUDFRONT= process.env.AWS_CLOUDFRONT
-
 # Load nvm (node version manager), install node (version in .nvmrc), and npm install packages
 [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh" && nvm use
 # Npm install if not already.
