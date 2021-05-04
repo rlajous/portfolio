50205fa5660ece52816fffb7cadab7719a378fc0
rlajous <rodrigo@lajous.com.ar>
2021-05-04 20:37:09 -0300

    Updated gulp deploy

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,7 @@ const config = {
   cacheFileName: '.awspublish',
   concurrentUploads: 10,
   wait: true, // wait for CloudFront invalidation to complete (about 30-60 seconds)
+  originPath: '*',
 }
 
 gulp.task('deploy', function () {
